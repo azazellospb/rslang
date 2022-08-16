3418b26ff139069fcb791b0a62a183001d287272
matvey84 <matvey8407@mail.ru>
2022-08-16 09:08:09 +0300

    init: first commit and install to oodependecies @reduxjs/toolkit @types/react-redux redux-devtools-extension

---
+++ src/components/redux/store.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/prefer-default-export */
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+// import wordDataReducer from './reducers/wordSlice'
+
+const rootReducer = combineReducers({
+  // wordDataReducer,
+})
+
+export const setupStore = () => configureStore({
+  reducer: rootReducer,
+})
+
+export type RootReducerState = ReturnType<typeof rootReducer>
+export type AppStoreState = ReturnType<typeof setupStore>
+export type AppDispatchState = AppStoreState['dispatch']
