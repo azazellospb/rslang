b52a64fb903ff26b2e49edb1ca91252a59120e8a
Anton Ivanov <aivanov.mcu@gmail.com>
2022-08-16 15:15:00 +0300

    feat: header with styling(w/0 auth part)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import styles from './Navigation.module.css'
+import styles from './Header.module.css'
 import Navigation from './Navigation'
+import Logo from './Logo'
 
 export default function Header() {
   return (
     <header className={styles.header}>
+      <Logo />
       <Navigation />
     </header>
   )
--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -7,12 +7,17 @@ import styles from './Navigation.module.css'
 export default function Navigation() {
   return (
     <nav className={styles.navigation}>
-      <ul>
-        <li><Link to="/dictionary">Словарь</Link></li>
-        <li><Link to="/audiochallenge">Аудиовызов</Link></li>
-        <li><Link to="/sprintchallenge">Спринт</Link></li>
-        <li><Link to="/stats">Статистика</Link></li>
-        <li><Link to="/about">О команде</Link></li>
+      <ul className={styles.menuList}>
+        <Link to="/dictionary"><li>Учебник</li></Link>
+        <div className={styles.dropdown}>
+          <button className={styles.dropbtn} type="button">Игры</button>
+          <div className={styles.dropdownContent}>
+            <Link to="/audiochallenge">Аудиовызов</Link>
+            <Link to="/sprintchallenge">Спринт</Link>
+          </div>
+        </div>
+        <Link to="/stats"><li>Статистика</li></Link>
+        <Link to="/about"><li>О команде</li></Link>
       </ul>
     </nav>
   )
