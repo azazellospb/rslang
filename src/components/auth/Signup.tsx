521e220f1414d75e9b7ee930f4e19ea0bc099cd5
MaxD <max.moderation@gmail.com>
2022-08-16 10:51:16 +0300

    feat: add signup & insert to header

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,16 +1,52 @@
 import React, { useState } from 'react'
 
-function Signup() {
-  const [value, setValue] = useState('')
-  // const userMail = useState();
+const URL = 'http://localhost:8088'
 
-  const handleSubmit = (event: React.FormEvent) => {
+function Signup() {
+  const [username, setName] = useState('')
+  const [usermail, setMail] = useState('')
+  const [userpassw, setPass] = useState('')
+
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
-    console.log('------form submitted-------')
+    setName('')
+    setMail('')
+    setPass('')
+
+    const response = await fetch(`${URL}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: username,
+        email: usermail,
+        password: userpassw,
+      }),
+    })
+
+    if (response.status === 417) {
+      alert('User with this e-mail exists')
+    }
+
+    const resp = await fetch(`${URL}/signin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: usermail,
+        password: userpassw,
+      }),
+    })
+
+    const userReg = await resp.json()
+    localStorage.setItem('userInfo', JSON.stringify(userReg))
+    alert(JSON.stringify(userReg))
   }
 
   return (
-    <div className="signup-wrapper">
+    <section className="signup-wrapper">
       <h3 className="signup">Регистрация</h3>
       <form className="signup" onSubmit={handleSubmit}>
         <div className="form-group">
@@ -21,24 +57,51 @@ function Signup() {
             type="text"
             id="username"
             placeholder="Введите имя..."
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
+            autoComplete="off"
+            onChange={(event) => setName(event.target.value)}
+            value={username}
+            required
           />
         </div>
         <div className="form-group">
-          <input type="text" id="usermail" placeholder="Email..." />
+          <input
+            type="text"
+            id="usermail"
+            placeholder="Email..."
+            autoComplete="off"
+            onChange={(event) => setMail(event.target.value)}
+            value={usermail}
+            required
+          />
         </div>
         <div className="form-group">
-          <input type="password" id="password" placeholder="Пароль 8 - 12 знаков" />
+          <input
+            type="password"
+            id="password"
+            placeholder="Пароль 8 - 12 знаков"
+            value={userpassw}
+            onChange={(event) => setPass(event.target.value)}
+            required
+          />
         </div>
-        {/* <div className="form-group"> */}
-        <button type="submit" className="btn registration">
-          Зарегистрироваться
-        </button>
-        {/* </div> */}
+        <div className="form-group">
+          <button type="submit" className="btn registration">
+            Регистрация
+          </button>
+        </div>
       </form>
-    </div>
+      <p>Сюда надо вставить</p>
+    </section>
   )
 }
 
 export default Signup
+// interface IUser {
+//   name: string
+//   email: string
+//   password: string
+// }
+// interface ISigninUser {
+//   errors: string[]
+//   status: string
+// }
