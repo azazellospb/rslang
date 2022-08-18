9e53aae2fdd2ade9069220f1754519c2db169c65
MaxD <max.moderation@gmail.com>
2022-08-18 13:54:05 +0300

    feat: add two forms & him styles

--- src/components/auth/Signin.tsx
+++ src/components/auth/Signin.tsx
@@ -4,67 +4,49 @@ import styles from './Auth.module.css'
 const URL = 'http://localhost:8088'
 /* eslint-disable react/destructuring-assignment */
 export default function Signin(props: { switchForm: (arg0: boolean) => void }) {
-  const [username, setName] = useState('')
   const [usermail, setMail] = useState('')
   const [userpassw, setPass] = useState('')
   const [userInf, setUserInf] = useState('')
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
-    setName('')
     setMail('')
     setPass('')
     setUserInf('')
-    let statusRespone = 0
+
     try {
-      const response = await fetch(`${URL}/users`, {
+      const response = await fetch(`${URL}/signin`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          name: username,
           email: usermail,
           password: userpassw,
         }),
       })
-      statusRespone = response.status
 
-      if (statusRespone === 200) {
-        setUserInf('Успех! Можете войти в учетную запись!')
-      }
-      if (statusRespone === 422) setUserInf('Введенные данные неверны!')
-      if (statusRespone === 417) setUserInf('Пользователь с таким \'email\' существует')
-      console.log(statusRespone)
+      const userIn = await response.json()
+      localStorage.setItem('userInfo', JSON.stringify(userIn))
+
+      if (response.status === 200) setUserInf('Вход выполнен успешно!')
     } catch (e) {
-      setUserInf('Введенные данные неверны!')
+      // console.log(e.message)
+      setUserInf('Email или пароль неверны!')
     }
   }
+
   /* eslint-disable  @typescript-eslint/no-explicit-any */
   function handleSwitch(e: any) {
     e.preventDefault()
 
-    props.switchForm(true)
+    props.switchForm(false)
   }
 
   return (
     <section className={styles.wrapper}>
-      <h2 className={styles.title}>Регистрация</h2>
+      <h2 className={styles.title}>Вход в аккаунт</h2>
       <form className={styles.signup} onSubmit={handleSubmit}>
-        {/* <div className="form-group">
-          <input type="file" accept="image/*" id="avatar" />
-        </div> */}
-        <div className="form-group">
-          <input
-            type="text"
-            id="username"
-            placeholder="Введите имя..."
-            autoComplete="off"
-            onChange={(event) => setName(event.target.value)}
-            value={username}
-            required
-          />
-        </div>
         <div className="form-group">
           <input
             type="text"
@@ -88,10 +70,10 @@ export default function Signin(props: { switchForm: (arg0: boolean) => void }) {
         </div>
         <div className={styles.btnContainer}>
           <a href="/" className={styles.btnSwitch} onClick={handleSwitch}>
-            Войти
+            Регистрация
           </a>
           <button type="submit" className={styles.btn}>
-            Регистрация
+            Войти
           </button>
         </div>
       </form>
