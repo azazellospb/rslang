a5a0cb926621a9e7bb1ad21240e39a4d76e43873
matvey84 <matvey8407@mail.ru>
2022-08-19 23:52:17 +0300

    refactor: 1) refactor import action in wordSlice file and change import and  usage actoin fron reducers

--- src/components/redux/fetching.ts
+++ src/components/redux/fetching.ts
@@ -2,7 +2,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-console */
 import { AppDispatchState } from './store'
-import { wordSlice } from './reducers/wordSlice'
+import { fetchWordSuccess } from './reducers/wordSlice'
 import { IWord } from '../../types/models'
 import { IFetchParam } from '../../types/sprint-game-models'
 
@@ -12,7 +12,7 @@ const getWordsData = () => async (dispatch: AppDispatchState) => {
     // TODO: сделать лоадер
     const response: Response = await fetch('http://localhost:8088/words/')
     const data: IWord[] = await response.json()
-    dispatch(wordSlice.actions.fetchWordSuccess(data))
+    dispatch(fetchWordSuccess(data))
   } catch (e) {
     console.log(e)
     // TODO: ОБОАБОТАТЬ ОШИБКУ
@@ -28,7 +28,7 @@ export const getWordsDataForSprintGame = (paramForFetch: IFetchParam) => async (
     const response: Response = await fetch(`${url}/?group=${textbookSection}&page=${page}`)
     const data: IWord[] = await response.json()
     console.log(data)
-    dispatch(wordSlice.actions.fetchWordSuccess(data))
+    // dispatch(fetchWordSuccess(data))// поменять экшен
   } catch (err) {
     console.log(err)
     // TODO: ОБОАБОТАТЬ ОШИБКУ
