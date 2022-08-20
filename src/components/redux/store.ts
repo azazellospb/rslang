b8e1b95c62c2a9432a220cbe460fb376cfd2e40e
MaxD <max.moderation@gmail.com>
2022-08-20 23:52:47 +0300

    feat: add lint exception rule & userLoginReducer

--- src/components/redux/store.ts
+++ src/components/redux/store.ts
@@ -2,15 +2,18 @@
 /* eslint-disable import/prefer-default-export */
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import wordDataReducer from './reducers/wordSlice'
+import userLoginReducer from './reducers/UserSlice'
 
 const rootReducer = combineReducers({
   wordDataReducer,
+  userLoginReducer,
 })
-
-export const setupStore = () => configureStore({
-  reducer: rootReducer,
-  devTools: true,
-})
+/* eslint-disable implicit-arrow-linebreak */
+export const setupStore = () =>
+  configureStore({
+    reducer: rootReducer,
+    devTools: true,
+  })
 
 export type RootReducerState = ReturnType<typeof rootReducer>
 export type AppStoreState = ReturnType<typeof setupStore>
