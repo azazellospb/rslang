d092d9ad61ceedba437ecff98e3364f516cf5b55
MaxD <max.moderation@gmail.com>
2022-08-21 01:15:02 +0300

    feat: add endpoints-path folder

---
+++ src/endpoints/endpoints.ts
@@ -0,0 +1,10 @@
+const URL = 'http://localhost:8088'
+
+const Endpoints = {
+  ROOT: `${URL}`,
+  USERS: `${URL}/users`,
+  WORDS: `${URL}/words`,
+  SIGNIN: `${URL}/signin`,
+}
+
+export default Endpoints
