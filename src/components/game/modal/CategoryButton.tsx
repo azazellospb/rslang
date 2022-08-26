82d9f66dd1e242477e705ec82bf4d1c33545aa9a
MaxD <max.moderation@gmail.com>
2022-08-26 18:15:55 +0300

    feat: remove comments code

--- src/components/game/modal/CategoryButton.tsx
+++ src/components/game/modal/CategoryButton.tsx
@@ -1,21 +1,14 @@
 /* eslint-disable import/no-cycle */
 /* eslint-disable no-console */
 import React from 'react'
-import { /* IFetchParam, */ IProp } from '../../../types/sprint-game-models'
-// import { getWordsDataForSprintGame } from '../../redux/fetching'
+import { IProp } from '../../../types/sprint-game-models'
 import { useAppDispatch } from '../../redux/hooks/redux'
 import { choiceCategory } from '../sprint-game/sprint-game-actions'
 import styles from './GameModal.module.css'
 
-function CategoryButton({ id, num }:IProp) {
+function CategoryButton({ id, num }: IProp) {
   const dispatch = useAppDispatch()
-  // const choiceCategory = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-  //   const paramForFetch: IFetchParam = {
-  //     textbookSection: (e.target as HTMLButtonElement).id,
-  //     page: Math.floor(Math.random() * 30),
-  //   }
-  //   dispatch(getWordsDataForSprintGame(paramForFetch))
-  // }
+
   return (
     <button
       id={String(id)}
