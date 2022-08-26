654f2280fda06ffa7e01f58fe1bf981e8418af9f
MaxD <max.moderation@gmail.com>
2022-08-26 18:12:40 +0300

    feat: add audio-reducer, refactor name wordSlice

--- src/components/redux/store.ts
+++ src/components/redux/store.ts
@@ -2,13 +2,15 @@
 /* eslint-disable import/prefer-default-export */
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import sprintGameSlice from './reducers/sprintGameSlice'
-import wordDataReducer from './reducers/wordSlice'
+import wordSlice from './reducers/wordSlice'
 import userReducer from './reducers/userSlice'
+import audioGameSlice from './reducers/audioGameSlice'
 
 const rootReducer = combineReducers({
-  wordDataReducer,
+  wordSlice,
   sprintGameSlice,
   userReducer,
+  audioGameSlice,
 })
 /* eslint-disable implicit-arrow-linebreak */
 export const setupStore = () =>
