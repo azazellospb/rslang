7ce81df18a5dd9c1abf3dd4c67fe76116fa08875
MaxD <max.moderation@gmail.com>
2022-08-30 14:07:27 +0300

    feat: add reset game when click book-btn

--- src/components/game/audio-game/game-ui/book-btn/BackToBook.tsx
+++ src/components/game/audio-game/game-ui/book-btn/BackToBook.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import BookIcon from './BookIcon'
 import styles from '../../game-stat/AudiogameStat.module.css'
+import { useAppDispatch } from '../../../../redux/hooks/redux'
+import { audioGameSlice } from '../../../../redux/reducers/audioGameSlice'
+import { gameSlice } from '../../../../redux/reducers/gameSlice'
 
 function BackToBook() {
+  const dispatch = useAppDispatch()
+  const ToBookHandle = () => {
+    dispatch(audioGameSlice.actions.fetchCounterProgress(1))
+    dispatch(audioGameSlice.actions.fetchCounterWord(0))
+    dispatch(gameSlice.actions.fetchGameOver(false))
+  }
   return (
-    <div className={styles.toBookBtn}>
+    <button type="button" className={styles.toBookBtn} onClick={ToBookHandle}>
       {/* BackToBook */}
       <BookIcon />
-    </div>
+    </button>
   )
 }
 
