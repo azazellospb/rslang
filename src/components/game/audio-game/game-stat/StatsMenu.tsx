a1e77888677cff027bad31ab9354690a5de4b4b6
MaxD <max.moderation@gmail.com>
2022-08-30 23:59:02 +0300

    feat: moved Link-components to BackToBook

--- src/components/game/audio-game/game-stat/StatsMenu.tsx
+++ src/components/game/audio-game/game-stat/StatsMenu.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import BackToBook from '../game-ui/book-btn/BackToBook'
 import ReloadGame from '../game-ui/reload-btn/ReloadGame'
 import styles from './StatsMenu.module.css'
@@ -8,9 +7,7 @@ function StatsMenu() {
   return (
     <div className={styles.wrapper}>
       <ReloadGame />
-      <Link to="/dictionary/0/0">
-        <BackToBook />
-      </Link>
+      <BackToBook />
       {/* <BackToGame /> */}
     </div>
   )
