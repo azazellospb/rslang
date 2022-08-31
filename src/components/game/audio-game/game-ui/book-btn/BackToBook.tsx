59a85179dd8a39fb21f1fad64848c5d50ed25482
MaxD <max.moderation@gmail.com>
2022-08-31 16:59:24 +0300

    feat: add locic dackBtn from statistics

--- src/components/game/audio-game/game-ui/book-btn/BackToBook.tsx
+++ src/components/game/audio-game/game-ui/book-btn/BackToBook.tsx
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom'
 import BookIcon from './BookIcon'
 import styles from '../../game-stat/AudiogameStat.module.css'
 import { useAppDispatch, useAppSelector } from '../../../../redux/hooks/redux'
-import { audioGameSlice } from '../../../../redux/reducers/audioGameSlice'
 import { gameSlice } from '../../../../redux/reducers/gameSlice'
+import { audioGameSlice } from '../../../../redux/reducers/audioGameSlice'
 
 function BackToBook() {
   const dispatch = useAppDispatch()
   const ToBookHandle = () => {
+    dispatch(gameSlice.actions.fetchGameOver(false))
     dispatch(audioGameSlice.actions.fetchCounterProgress(1))
     dispatch(audioGameSlice.actions.fetchCounterWord(0))
-    dispatch(gameSlice.actions.fetchGameOver(false))
+    dispatch(audioGameSlice.actions.learnedWord({}))
   }
   const currentGroupPage = useAppSelector((state) => state.sprintGameSlice.currentGroupPage)
   return (
