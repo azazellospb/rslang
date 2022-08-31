481b3c3d017cf2c5c440fdb3a345fb8b5b7dc176
MaxD <max.moderation@gmail.com>
2022-08-31 13:14:18 +0300

    feat: change models interface IUnlearnedWord - _id: string

--- src/types/models.ts
+++ src/types/models.ts
@@ -61,7 +61,7 @@ export interface IAggregOrUserWord extends Omit<IWord, 'id'> {
 
 export interface IUnlearnedWord {
   id?: string
-  _id?: string
+  _id: string
   group: number
   page: number
   word: string
