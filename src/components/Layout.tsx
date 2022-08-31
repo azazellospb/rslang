f98ca184aab56de900a7992d3f2758f9930ca7c7
MaxD <max.moderation@gmail.com>
2022-08-31 22:44:57 +0300

    feat: add footer styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import Footer from './footer/Footer'
 import Header from './header/Header'
 
 export default function Layout() {
