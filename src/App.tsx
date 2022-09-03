c8a7fbd96bf219f09140a318bc3556046c1e62f1
Anton Ivanov <aivanov.mcu@gmail.com>
2022-09-03 05:03:05 +0300

    fix: fix reload issue for stats page

--- src/App.tsx
+++ src/App.tsx
@@ -29,8 +29,8 @@ function App() {
   const month = (date.getMonth() + 1).toString().length !== 1 ? (date.getMonth() + 1).toString() : `0${(date.getMonth() + 1).toString()}`
   const dateKey = `d${date.getDate().toString()}${month}${date.getFullYear().toString()}`
   useEffect(() => {
-    dispatch(getWordsData())
     dispatch(getUserStats())
+    dispatch(getWordsData())
     dispatch(getLearnedWithDates())
     dispatch(getTodayLearned(dateKey))
   }, [dispatch, dateKey])
