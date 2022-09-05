3505022cecdb148b8333ab093f01f0ec01ddbad1
Anton Ivanov <aivanov.mcu@gmail.com>
2022-09-05 22:06:05 +0300

    Updates

--- src/components/redux/hooks/redux.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import { AppDispatchState, RootReducerState } from '../store'
-
-export const useAppDispatch = () => useDispatch<AppDispatchState>()
-export const useAppSelector: TypedUseSelectorHook<RootReducerState> = useSelector
--- src/pages/Page404.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function AudioChallenge() {
-  return (<h2>Ошибка. Такой страницы не существует!</h2>)
-}
